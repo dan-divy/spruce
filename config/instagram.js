d9248439d55fecae923c05bc7f25f5c70d8edac4
Dan <dan@dancodes.online>
2019-04-13 15:58:29 +0530

    small bug

--- config/instagram.js
+++ config/instagram.js
@@ -1,6 +1,6 @@
 var in_client_id = '7f1419930e724ffea8642d1f4cc70a12',
 	in_client_secret = '944ac21b95704d68a77d638873bd1622',
-	in_redirect_uri = 'http://localhost/account/oauth',
+	in_redirect_uri = 'http://mygurukulonline.in/account/oauth',
 	in_auth_url = 'https://api.instagram.com/oauth/authorize/?client_id=' + in_client_id + '&redirect_uri=' + in_redirect_uri + '&response_type=code'
 
 module.exports = {
