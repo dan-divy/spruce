dd86dbc3f81999871f88be793847aaf3648ea64c
Dan <dan@dancodes.online>
2019-04-20 15:41:04 +0530

    some small updates

--- config/instagram.js
+++ config/instagram.js
@@ -1,6 +1,6 @@
-var in_client_id = process.env.i_client_id || '<CLIENT_ID>',
-	in_client_secret = process.env.i_secret ||'<CLIENT_SECRET>',
-	in_redirect_uri = process.env.i_redirect_uri || '<host>:<port>/account/oauth/instagram',
+var in_client_id = '<client_id>',
+	in_client_secret = '<client_secret>',
+	in_redirect_uri = 'http://localhost/account/oauth/instagram', // Check your port...
 	in_auth_url = 'https://api.instagram.com/oauth/authorize/?client_id=' + in_client_id + '&redirect_uri=' + in_redirect_uri + '&response_type=code'
 
 module.exports = {
