8473f4602184c3eb970af1923c9418155b0f71a8
MayorChano <Dannyrwilson@me.com>
2019-04-21 14:33:58 +0200

    Fix insta

--- config/instagram.js
+++ config/instagram.js
@@ -4,12 +4,8 @@ var in_client_id = process.env.i_client_id || '<CLIENT_ID>',
 	in_auth_url = 'https://api.instagram.com/oauth/authorize/?client_id=' + in_client_id + '&redirect_uri=' + in_redirect_uri + '&response_type=code'
 
 module.exports = {
-
-	instagram: {
 		client_id: in_client_id,
 		client_secret: in_client_secret,
 		auth_url: in_auth_url,
 		redirect_uri: in_redirect_uri
-	}
-
 };
