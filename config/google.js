3096b23cf8c15c51a5a6f9e37ca5bb52e54621f7
MayorChano <Dannyrwilson@me.com>
2019-04-21 14:27:53 +0200

    Fixed liking system completely

--- config/google.js
+++ config/google.js
@@ -1,6 +1,6 @@
-var client_id = process.env.g_client_id || '<CLIENT_ID>',
-	client_secret = process.env.g_client_secret || '<CLIENT_SECRET>',
-    redirect_uri = process.env.g_redirect_uri || '<host>:<port>/account/oauth/google',
+var client_id = process.env.g_client_id || '977873307903-bpjrfdup4h8e88nrrjoib39j9h002o7m.apps.googleusercontent.com',
+	client_secret = process.env.g_client_secret || 'OSqMXvVzsmkxJJBiTmdkwiRt',
+    redirect_uri = process.env.g_redirect_uri || 'http://localhost/account/oauth/google',
     auth_url = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=' + redirect_uri + '&scope=profile%20email&client_id=' + client_id
 
 module.exports = {
