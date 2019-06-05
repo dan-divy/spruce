c7551a38f73658f749eaa5681b260e700f699cb2
Dan <dan@dancodes.online>
2019-06-05 14:43:41 +0530

    Updated chat stuff [WIP]

--- utils/models/room.js
+++ utils/models/room.js
@@ -3,7 +3,10 @@
 var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 // define the schema for our user model
+mongoose.connect(require('../../config/app').db.connectionUri);
+
 var chatSchema = mongoose.Schema({
+    id: String, // "12ojahsdbi2qwbdoihabfqyyegr8uyadf823798w791" Combined _id of users
     users:Array, // ["John", "Doe"]
     chats:Array // {txt:"Hi", by:"john", time:"10:35pm"}
 });
