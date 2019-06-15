a67b8287b203e3201093fe695ba15f3e2a22d4a3
Divy Srivastava <dj.srivastava23@gmail,com>
2019-06-15 10:39:18 +0530

    socket backend structure
    Sorry for being late

--- utils/handlers/socket.js
+++ utils/handlers/socket.js
@@ -1,16 +1,41 @@
 const io = require('socket.io');
 const express = require('express');
-const server = require('../../bin/www')
-var sio = io(server);
+var Room = require("../models/room");
+const {sio} = require('../../bin/www'); 
 const sessionMiddleware = require('../../app').sessionMiddleware;
 
 sio.use(function(socket, next) {
     sessionMiddleware(socket.request, socket.request.res, next);
 });
 
+function sendMsg(socket, chat) {
+    var time = new Date();
+    var room = socket.room;
+    room.chats.push({txt:chat.txt, by: socket.request.session.user._id, time})
+    room.save((err,obj) => {
+        return true;
+        socket.broadcast.emit("new msg", {
+            txt:chat.txt,
+            by:socket.request.session.user.username,
+            time   
+        });
+    });
+};
+
 sio.on("connection", function(socket) {
-    console.log(socket.request.session);
-    socket.on('')
+    Room
+    .find({room: socket.request.session.socket.room}, function (err, room) {
+        if(!room) {
+            return socket.disconnect('unauthorized');
+        }
+        socket.room = room; 
+    });
+    socket.on('msg', function(data) {
+        sendMsg(socket, data);        
+    });
 });
 
+/**
+ * Pushpushpushpushpush
+ */
 module.exports = io;
