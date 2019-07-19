0d05da8d62366cbda24a3ba8dc7b1364a839cc95
Dan <dan@dancodes.online>
2019-07-19 12:46:53 +0200

    newUrlParser err msg

--- utils/models/room.js
+++ utils/models/room.js
@@ -1,16 +1,18 @@
 // app/models/room.js
 // load the things we need
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
 // define the schema for our user model
-mongoose.connect(require('../../config/app').db.connectionUri);
-
-var chatSchema = mongoose.Schema({
-    id: String, // "12ojahsdbi2qwbdoihabfqyyegr8uyadf823798w791" Combined _id of users
-    users:Array, // ["John", "Doe"] 
-    chats:Array // {txt:"Hi", by:"john", time:"10:35pm"}
+mongoose.connect(require("../../config/app").db.connectionUri, {
+  useNewUrlParser: true
 });
 
-module.exports = mongoose.model('room', chatSchema);
+var chatSchema = mongoose.Schema({
+  id: String, // "12ojahsdbi2qwbdoihabfqyyegr8uyadf823798w791" Combined _id of users
+  users: Array, // ["John", "Doe"]
+  chats: Array // {txt:"Hi", by:"john", time:"10:35pm"}
+});
+
+module.exports = mongoose.model("room", chatSchema);
 
 // create the model for users and expose it to our app
