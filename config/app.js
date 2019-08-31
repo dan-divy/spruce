3b9dd6375404a0eb8771b7c07383bb3ea76f784c
Robert <robert_burckner@yahoo.com>
2019-08-31 12:24:30 +0300

    baseline

--- config/app.js
+++ config/app.js
@@ -1,19 +1,19 @@
 var dbHost = process.env.dbHost || "localhost";
 module.exports = {
-	"name":"spruce",
-	"title":"spruce",
+	"name":"oak",
+	"title":"oak",
 	"commands":{
-		"package":"electron-packager electron.js spruce --electronVersion=2.0.12 --overwrite --icon=/public/images/logo/logo.png --prune=true --out=release",
+		"package":"electron-packager electron.js oak --electronVersion=2.0.12 --overwrite --icon=/public/images/logo/logo.png --prune=true --out=release",
 		"build":""
 	},
 	"http": {
 		"host":"localhost",
-		"port":8000
+		"port":8080
 	},
-	"author":"Divy Srivastava",
-	"version":"2.0.0",
+	"author":"Robert Burckner",
+	"version":"0.1.0",
 	"db": {
-		"connectionUri":"mongodb://"+dbHost+":27017/spruce",
+		"connectionUri":"mongodb://"+dbHost+":27017/oak",
 		"params": {},
 		"collections": [
 			"moment",
