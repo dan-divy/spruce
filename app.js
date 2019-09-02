d3f1e243ff8df5a0191c22eaa94b5ceaa1533308
Robert <robert_burckner@yahoo.com>
2019-09-02 20:15:57 +0300

    baseline2

--- app.js
+++ app.js
@@ -1,53 +1,121 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var session = require("express-session");
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+'use strict';
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const expressSession = require('express-session');
+const helmet = require('helmet');
+const mongoose = require("mongoose");
+const path = require('path');
+const redis = require('redis')
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var accountRouter = require("./routes/auth");
-var meRouter = require("./routes/settings");
-var extraRouter = require("./routes/extras/wordbeater/main");
-var categoryRouter = require("./routes/category");
-var restApi = require("./routes/api/v1/index");
-var publicApiRouter = require("./routes/developer/api");
-var chatRouter = require("./routes/chat");
+const pkg = require('./package.json');
+const protection = require('./utils/middleware/protection');
 
-var app = express();
-app.conf = require("./config/app");
+const accountRouter = require('./routes/auth');
+const categoryRouter = require('./routes/category');
+const chatRouter = require('./routes/chat');
+const extraRouter = require('./routes/extras/wordbeater/main');
+const indexRouter = require('./routes/index');
+const meRouter = require('./routes/settings');
+const usersRouter = require('./routes/users');
+
+const publicApiRouter = require('./routes/developer/api');
+const restApiV1file = require('./routes/api/v1/file');
+const restApiV1community = require('./routes/api/v1/community');
+const restApiV1index = require('./routes/api/v1/index');
+
+const app = express();
+app.conf = require('./config/app');
+
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-var cooky = {
-  secret: "work hard",
-  resave: true,
-  expires: new Date() * 60 * 60 * 24 * 7,
-  saveUninitialized: true
-};
+// Define environment
+const NODE_ENV = process.env.NODE_ENV || app.conf.env || 'development';
+app.set('env', NODE_ENV);
+const isDev = NODE_ENV === 'development';
 
-app.sessionMiddleware = session(cooky);
+mongoose.connect(require(path.join(__dirname, 'config/app')).db.connectionUri, {
+  useNewUrlParser: true
+}, (err) => {
+  if (err) {
+    console.log(`ERROR connecting to DB: ${app.conf.db.host}`);
+    return process.exit(1);
+  }
+  console.log(`Successfully connected to DB: ${app.conf.db.host}`);
+});
 
-app.set("trust proxy", 1); // trust first proxy
-app.use(app.sessionMiddleware);
-app.use(logger("tiny"));
+var session;
+if (isDev) {
+  console.log('Operating in DEVELOPMENT mode.');
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+  // SESSION - Use FileStore in development mode.to reduce the authentication burdon.
+  const fileStore = require('session-file-store')(expressSession);
+  const redisClient = redis.createClient();
+  session = expressSession ({
+    resave: app.conf.cookie.resave,
+    saveUninitialized: app.conf.cookie.saveUninitialized,
+    secret: app.conf.cookie.secret,
+    expires: app.conf.cookie.expiresIn,
+    store: new fileStore(),
+  });
+} else {
+  // SESSION - Use RedisStore in production mode.
+  const redisStore = require('connect-redis')(expressSession);
+  const redisClient = redis.createClient();
+  session = expressSession({
+    resave: app.conf.cookie.resave,
+    saveUninitialized: app.conf.cookie.saveUninitialized,
+    secret: app.conf.cookie.secret,
+    expires: app.conf.cookie.expiresIn,
+    store: new redisStore({
+      host: app.conf.redis.host,
+      port: app.conf.redis.port,
+      client: redisClient
+    }),
+  });
+  app.use(compression());
+  app.use(helmet());
+} 
+app.use(session);
+app.sessionMiddleware = session;
+// End Session setup
+
+app.set('trust proxy', 1); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/u", usersRouter);
-app.use("/account", accountRouter);
-app.use("/me", meRouter);
-app.use("/api", restApi);
-app.use("/category", categoryRouter);
-app.use("/products", extraRouter);
-app.use("/chat", chatRouter);
-app.use("/developer", publicApiRouter);
+// Routes
+app.get('/version', (req, res) => {
+  res.status(200).json({ 
+    version: pkg.version,
+    mode: app.node_env,
+  });
+});
+
+// Public routes
+app.use('/', indexRouter);
+app.use('/account', accountRouter);
+
+// Secure routes
+//app.use(protection.isAuthenticated);
+
+app.use('/u', usersRouter);
+app.use('/me', meRouter);
+app.use('/category', categoryRouter);
+app.use('/products', extraRouter);
+app.use('/chat', chatRouter);
+
+app.use('/api/v1', restApiV1index);
+app.use('/api/v1/community', restApiV1community);
+app.use('/api/v1/file', restApiV1file);
+app.use('/developer', publicApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,11 +126,13 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error', {
+    title: req.app.conf.name,
+  });
 });
 
 module.exports = app;
--- config/app.js
+++ config/app.js
@@ -5,46 +5,46 @@ var dbUsername = "";
 var dbPassword = "";
 var dbCredentials = "";
 if (dbAuth) {
-  dbUsername = process.env.dbUsername || "sheldon";
-  dbPassword = process.env.dbPassword || "supersecret";
-  dbCredentials = dbUsername + ":" + dbPassword + "@";
+	dbUsername = process.env.dbUsername || "sheldon";
+	dbPassword = process.env.dbPassword || "supersecret";
+	dbCredentials = dbUsername + ":" + dbPassword + "@";
 }
 const appSecret = process.env.appSecret || "appSecret";
 
 module.exports = {
-  "env": "development", // production or development
-  "name":"spruce",
-  "title":"spruce",
-  "privateFiles": "files",
-  "commands": {
-    "package": "electron-packager electron.js oak --electronVersion=2.0.12 --overwrite --icon=/public/images/logo/logo.png --prune=true --out=release",
-    "build": ""
-  },
-  "http": {
-    "host": "localhost",
-    "port": 8080
-  },
-  "author": "Robert Burckner",
-  "version": "0.1.4",
-  "db":  {
-    "connectionUri": "mongodb://" + dbCredentials + dbHost + ":27017/oak",
-    "host": dbHost,
-    "params": {},
-    "collections": [
-      "moment",
-      "user",
-      "feeling",
-      "ask"
-    ]
-  },
-  "cookie": {
-    "resave": true,
-    "secret": appSecret,
-    "expiresIn": new Date() * 60 * 60 * 24 * 7,
-    "saveUninitialized": true
-  },
-  "redis": {
-    "host": "localhost",
-    "port": 6379
+	"env": "development", // production or development
+	"name": "oak",
+	"title": "oak",
+	"privateFiles": "files",
+	"commands": {
+		"package": "electron-packager electron.js oak --electronVersion=2.0.12 --overwrite --icon=/public/images/logo/logo.png --prune=true --out=release",
+		"build": ""
+	},
+	"http": {
+		"host": "localhost",
+		"port": 8080
+	},
+	"author": "Robert Burckner",
+	"version": "0.1.4",
+	"db":  {
+		"connectionUri": "mongodb://" + dbCredentials + dbHost + ":27017/oak",
+		"host": dbHost,
+		"params": {},
+		"collections": [
+			"moment",
+			"user",
+			"feeling",
+			"ask"
+		]
+	},
+	"cookie": {
+		"resave": true,
+	  "secret": appSecret,
+	  "expiresIn": new Date() * 60 * 60 * 24 * 7,
+	  "saveUninitialized": true
+	},
+	"redis": {
+  	"host": "localhost",
+  	"port": 6379
   }
 }
