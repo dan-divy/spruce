9037faa39ddc13cf057fbc40868f268ce12d6d08
DanCodes <dan@dancodes.online>
2019-09-16 15:19:52 +0200

    get rid of console log

--- test/database_tests.js
+++ test/database_tests.js
@@ -23,7 +23,6 @@ describe('Database Tests', function() {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error'));
     db.once('open', function() {
-      console.log('We are connected to test database!');
       done();
     });
   });
